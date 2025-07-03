ec874970e00fdd34d62b23bc48d7763d21863900
wally_yoon <bluegmlduf2@naver.com>
2025-07-03 21:21:10 +0900

    feat: package.json 및 postcss.config.mjs에 Tailwind CSS 관련 패키지 추가

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,8 +1,6 @@
-import tailwindcss from "@tailwindcss/postcss";
-import autoprefixer from "autoprefixer";
-
+/** @type {import('postcss-load-config').Config} */
 const config = {
-    plugins: [tailwindcss, autoprefixer],
+    plugins: ["@tailwindcss/postcss", "autoprefixer"],
 };
 
 export default config;
